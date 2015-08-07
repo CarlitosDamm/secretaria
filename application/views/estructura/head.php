7dd23b71aabfa3edda87e005c76fa707ae67ccc7
Chomo <iscalmeda@gmail.com>
2015-08-07 14:44:06 -0500

    Se agrego el CSS de JUI

--- application/views/estructura/head.php
+++ application/views/estructura/head.php
@@ -5,6 +5,7 @@
 		<meta http-equiv="X-UA-Compatible" content="IE=edge">
 		<meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
 		<title>| Secretaria General UJED |</title>
+		<link rel="stylesheet" href="https://ajax.googleapis.com/ajax/libs/jqueryui/1.11.2/themes/smoothness/jquery-ui.css" />
 	    <link href="<?=base_url()?>includes/css/estilo.css" rel="stylesheet">
 	    <link href="<?=base_url()?>includes/iconos.css" rel="stylesheet">
 
--- application/views/usuarios/inicio.php
+++ application/views/usuarios/inicio.php
@@ -4,45 +4,19 @@
 
 <div class="row">
 	<div class="full centrado">
-		<input type="text" class="buscar">
-		<input type="submit" class="boton" value="Buscar">
+		<input type="text" class="buscar" id="Fecha">
+		<input type="submit" class="boton" value="Buscar" >
 	</div>
 </div>
 <div class="row">
 	<div class="full">
-		<table class="tablaAgenda">
-			<tr>
-				<th>
-					Nombre
-				</th>
-				<td>
-					Ningun nombre
-				</td>
-			</tr>
-			<tr>
-				<th>
-					Nombre
-				</th>
-				<td>
-					Ningun nombre
-				</td>
-			</tr>
-			<tr>
-				<th>
-					Nombre
-				</th>
-				<td>
-					Ningun nombre
-				</td>
-			</tr>
-			<tr>
-				<th>
-					Nombre
-				</th>
-				<td>
-					Ningun nombre
-				</td>
-			</tr>
-		</table>
+		<section>
+			<article class="tabla">
+				<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Atque ipsum eaque voluptate, illo, quasi tempora assumenda deleniti sapiente animi iure cumque velit ut voluptatem, nostrum explicabo sint saepe pariatur. Iure.</p>
+			</article>
+			<article class="tabla">
+				<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Perferendis consequatur, modi unde labore officia eum, doloremque consequuntur, architecto facilis esse nemo nulla velit. Quo similique, non quis eius quibusdam quam?</p>	
+			</article>
+		</section>
 	</div>
 </div>
