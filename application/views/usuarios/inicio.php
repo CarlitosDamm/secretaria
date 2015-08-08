97c7a5f87ea1a38d57cdcc6f37d5f5cef72a7147
ChomoAlmeda <iscalmeda@gmail.com>
2015-08-08 07:56:14 -0500

    Se agregaron los titulos

--- application/views/usuarios/inicio.php
+++ application/views/usuarios/inicio.php
@@ -4,7 +4,7 @@
 
 <div class="row">
 	<div class="full centrado">
-		<input type="text" class="buscar" id="Fecha">
+		<input type="text" class="buscar" id="Fecha" placeholder="Buscar Fecha">
 		<input type="submit" class="boton" value="Buscar" >
 	</div>
 </div>
@@ -12,9 +12,11 @@
 	<div class="full">
 		<section>
 			<article class="tabla">
+				<h1>Agenda del Día</h1>
 				<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Atque ipsum eaque voluptate, illo, quasi tempora assumenda deleniti sapiente animi iure cumque velit ut voluptatem, nostrum explicabo sint saepe pariatur. Iure.</p>
 			</article>
 			<article class="tabla">
+				<h1>Documentos del Dia</h1>
 				<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Perferendis consequatur, modi unde labore officia eum, doloremque consequuntur, architecto facilis esse nemo nulla velit. Quo similique, non quis eius quibusdam quam?</p>	
 			</article>
 		</section>
